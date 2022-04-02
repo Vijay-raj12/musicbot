15d5dcdce4d5ef3d07601b9c3d2374de7469e2f6
Vijay-raj12 <97966081+Vijay-raj12@users.noreply.github.com>
2022-04-02 06:45:23 +0530

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,7 @@
 module.exports = async (client) => {
   console.log(`[API] Logged in as ${client.user.username}`);
   await client.user.setActivity("-help | HD MUSIC", {
-    type: "LISTENING",//can be LISTENING, WATCHING, PLAYING, STREAMING
+    type: "LISTENING",
+    status:"dnd"
   });
 };
