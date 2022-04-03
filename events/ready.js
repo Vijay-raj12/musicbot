2476c53bfc9b0064b8587cf4e2826b6dc88e0fc3
Vijay-raj12 <97966081+Vijay-raj12@users.noreply.github.com>
2022-04-03 07:17:28 +0530

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -2,6 +2,6 @@ module.exports = async (client) => {
   console.log(`[API] Logged in as ${client.user.username}`);
   await client.user.setActivity("-help | Invite Me ", {
     type: "PLAYING",
-    status:"dnd"
+    status:"dnd",
   });
 };
