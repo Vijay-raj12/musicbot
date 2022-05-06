9ae31a54a7f73cad73a5096b325b98fb83f44d22
Vijay-raj12 <97966081+Vijay-raj12@users.noreply.github.com>
2022-05-06 06:26:52 +0530

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,6 @@
 module.exports = async (client) => {
   console.log(`[API] Logged in as ${client.user.username}`);
-  await client.user.setActivity("-help | Invite Me ", {
+  await client.user.setActivity(":help | Invite Me ", {
     type: "LISTENING",
 
 
